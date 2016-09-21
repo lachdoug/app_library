8f7b01f8d30865730fa240561d38c6c88d2a573b
Lachlan Douglas <lachlan@engines.org>
2016-09-21 15:19:03 +1000

    wide fields

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -10,10 +10,14 @@ class AdminsController < ApplicationController
   def update
     @user = current_user
     respond_to do |format|
-      if @user.valid_password?(strong_params[:current_password]) && @user.update(strong_params.to_h.without('current_password'))
-        format.html { redirect_to apps_path, notice: "Successfully updated admin password."}
+      if @user.valid_password?(strong_params[:current_password])
+        if @user.update(strong_params.to_h.without('current_password'))
+          format.html { redirect_to apps_path, notice: "Successfully updated admin password."}
+        else
+          format.html { render :edit }
+        end
       else
-        format.html { render :edit }
+        format.html { render :edit, notice: "Incorrect current password." }
       end
     end
   end
